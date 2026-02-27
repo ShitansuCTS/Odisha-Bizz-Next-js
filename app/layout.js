c3ff0a245ef86aa137c7f871d6db680827336023
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-02-27 12:19:45 +0530

    Update the Search console , GTA and No script tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Welcome to Odisha Biz || Properties & Local Business Listings",
   description: "Explore verified property and business listings in Odisha including hotels, restaurants, travel, and commercial spaces.",
+  verification: { google: "NVR4AH5Lsgl3TVcyHrctrI9luhl9gIXlU0Wr4s4zfM8" },
   icons: { icon: "/logo-fav.ico" },
 };
 
@@ -22,7 +24,30 @@ import ClientLayout from "./ClientLayout";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Google Tag Manager - Head */}
+      <Script
+        id="gtm-head"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K9BG9KRM');
+          `,
+        }}
+      />
+
+
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K9BG9KRM"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
+          </iframe>
+        </noscript>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
