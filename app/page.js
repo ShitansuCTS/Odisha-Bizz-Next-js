301e8b3a6aa11b2a9b1fd263b20e28d6d4d3a54f
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-04-21 12:37:08 +0530

    Comment the Faq

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Collage />
       <Cities />
       <FeaturedServices />
-      <WhyChooseUs />
+      {/* <WhyChooseUs /> */}
       <Faq />
     </>
   );
